4fb2929d77d1608d4358ecd344676ad2ac38a88d
karanpreetjaggi <jkaranpreet@gmail.com>
2021-01-17 15:39:33 -0500

    Adding the election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -45,10 +45,46 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+total_votes = 0
 # Open the election results and read the file.
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ''
+winning_count = 0
+winning_percentage = 0
+
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
-    # Read and print the header row.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+# Print each row in the CSV file.
+    for row in file_reader:
+       total_votes += 1
+       candidate_name = row[2]
+       if candidate_name not in candidate_options:
+        candidate_options.append(candidate_name)
+        
+        candidate_votes[candidate_name] = 0
+    
+       candidate_votes[candidate_name] += 1
+
+# Print the candidate list.
+print(candidate_votes)
+
+for candidate_name in candidate_votes:
+    votes = candidate_votes[candidate_name]
+    #calculate percentage of votes
+    vote_percentage = float(votes)/ float(total_votes) * 100
+    if(votes > winning_count) and (vote_percentage > winning_percentage):
+        winning_count = votes
+        winning_percentage = vote_percentage
+        #set the winning_candidate equal to candidate_name
+        winning_candidate = candidate_name
+winning_candidate_summary = (
+    f"-----------\n"
+    f"Winner:{winning_candidate}\n"
+    f"winning count:{winning_count:,}\n"
+    f"winning percentage:{winning_percentage:.1f}%\n"
+    f"--------------\n")
+print(winning_candidate_summary)
